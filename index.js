59ed88cfc5bc0552c1334f77bc31e0c0f494c64d
Jerryomwesh <jerryomwesh@gmail.com>
2025-06-27 18:28:40 +0300

    Update writeCards, countDown functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// Function to generate thank-you messages
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+// Function to count down from a given positive integer to zero
+function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+        console.log(startingNumber);
+        startingNumber--; 
+    }
+}
+
+
+module.exports = {
+    writeCards,
+    countDown
+};
